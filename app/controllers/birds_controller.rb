0330d0c75600600ff32e9a2fcdf4e4ac999ccc4c
Matt <aboutmattlaw@gmail.com>
2021-11-01 11:56:42 -0400

    commit tree 1a601ae3c739ff9d886930ecf5b49b7a7e992373

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,12 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -14,6 +20,9 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
-  end
+
+
+
+end
 
 end
